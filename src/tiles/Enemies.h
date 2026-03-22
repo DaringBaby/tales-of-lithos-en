24e6ebd7da7383071480dd80ae41ccdeb3a1e1b3
Daring <80385125+DaringBaby@users.noreply.github.com>
2026-03-22 17:21:44 +0100

    Added tiles for other 8 enemies in the game.

--- src/tiles/Enemies.h
+++ src/tiles/Enemies.h
@@ -4,5 +4,15 @@
 extern const unsigned char LarvaOscura[];
 extern const unsigned char Trisguardo[];
 extern const unsigned char PredatoreOmbra[];
+extern const unsigned char Armatura[];
+extern const unsigned char Ragnocchio[];
+extern const unsigned char Pipistrello[];
+extern const unsigned char Dragocchio[];
+extern const unsigned char OcchioInfuocato[];
+extern const unsigned char Quadratocchio[];
+extern const unsigned char Cervellino[];
+extern const unsigned char Cristallocchio[];
+
+
 
 #endif
